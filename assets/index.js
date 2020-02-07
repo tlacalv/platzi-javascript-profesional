8908a14b35636964a8fc77b27bf86185f00f3aff
tlacalv <radeon777@live.com>
2020-02-07 12:35:37 -0700

    src implementation

---
+++ assets/index.js
@@ -0,0 +1,24 @@
+const video = document.querySelector("video")
+      const button = document.querySelector("button")
+
+      //"CLASS"
+      function MediaPlayer(config) {
+        this.media = config.el
+      }
+      MediaPlayer.prototype.play = function (){
+        this.media.play();
+      }
+      MediaPlayer.prototype.pause =function(){
+        this.media.pause();
+      }
+      MediaPlayer.prototype.togglePlay = function(){
+        if(this.media.paused){
+          this.play();
+        }else{
+          this.pause();
+        }
+      }
+
+      const player = new MediaPlayer({el: video});
+      
+      button.onclick =() => player.togglePlay();
