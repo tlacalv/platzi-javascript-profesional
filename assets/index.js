9f79f8f1cbec012cc057d89337eae036dc516984
tlacalv <radeon777@live.com>
2020-02-14 10:47:30 -0700

    modules implemented

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,9 @@
+import MediaPlayer from './MediaPlayer.js'
+
 const video = document.querySelector("video")
-      const button = document.querySelector("button")
+const player = new MediaPlayer({el: video});
 
-      //"CLASS"
-      function MediaPlayer(config) {
-        this.media = config.el
-      }
-      MediaPlayer.prototype.play = function (){
-        this.media.play();
-      }
-      MediaPlayer.prototype.pause =function(){
-        this.media.pause();
-      }
-      MediaPlayer.prototype.togglePlay = function(){
-        if(this.media.paused){
-          this.play();
-        }else{
-          this.pause();
-        }
-      }
+const button = document.querySelector("button")
 
-      const player = new MediaPlayer({el: video});
-      
-      button.onclick =() => player.togglePlay();
+
+button.onclick =() => player.togglePlay();
