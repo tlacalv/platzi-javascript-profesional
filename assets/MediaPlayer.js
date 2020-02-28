91321b9e89379192d58b673b5cc0892024368cbe
tlacalv <radeon777@live.com>
2020-02-28 13:13:01 -0700

    Get and set

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -8,8 +8,19 @@ function MediaPlayer(config) {
   }
   MediaPlayer.prototype._initPlugins = function(){
       //para cada instancia en el array de plugins corre el metodo run.
+      const player = {
+        play: ()=> this.play(),
+        pause: ()=> this.pause(),
+        media: this.media,
+        get muted(){
+          return this.media.muted;
+        },
+        set muted(value) {
+          this.media.muted= value;
+        } 
+      }
       this.plugins.forEach(plugin => {
-          plugin.run(this)
+          plugin.run(player)
       })
   }
   MediaPlayer.prototype.play = function (){
