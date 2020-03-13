c6af81928dc6ba27b5899c360c10cb97304715a2
tlacalv <radeon777@live.com>
2020-03-13 11:32:51 -0700

    Implementing decorator

--- ejercicios/decorator/index.ts
+++ ejercicios/decorator/index.ts
@@ -21,34 +21,33 @@ class Field {
 }
 
 function RequiredFieldDecorator(field: Field): Field {
-  let validate = field.validate;
-
-  field.validate = function() {
-    validate();
+  //guardamos funcion validate original 
+  let validate = field.validate
+  
+  field.validate = function(){
+    validate()
     let value = field.input.value;
-    if (!value) {
-      field.errors.push('Requerido');
+    if(!value){
+      field.errors.push("Requerido")
     }
-  };
-
+  }
   return field;
 }
 
 function EmailFieldDecorator(field: Field): Field {
-  let validate = field.validate;
-
-  field.validate = function() {
-    validate();
+  //guardamos funcion validate original 
+  let validate = field.validate
+  
+  field.validate = function(){
+    validate()
     let value = field.input.value;
-
-    if (value.indexOf('@') === -1) {
-      field.errors.push('Debe ser un email');
+    if(value.indexOf('@') === -1){
+      field.errors.push("Debe ser un email")
     }
-  };
-
+  }
   return field;
 }
 
 let field = new Field(document.querySelector('#email'));
 field = RequiredFieldDecorator(field);
-field = EmailFieldDecorator(field);
+field = EmailFieldDecorator(field)
